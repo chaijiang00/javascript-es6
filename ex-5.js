ea4534402c43d816ff68600a60a66d0d1ed32233
chaijiang <chaijiangchaisan@gmail.com>
2025-06-02 21:43:55 +0700

    fix : ex1-7

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,8 @@
-function sum() {
+function sum(...num) {
   // Start coding here !
+  let result = num.reduce((acc,cur) => acc+cur,0);
+  return result;
+  
 }
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
